950209638d4f43a90f6e98afad8b4c59fffa642e
Jarle Børsheim <jarle.borsheim@gmail.com>
2022-03-31 10:43:12 +0200

    Lagt til de siste kodelistene

---
+++ KS.Fiks.Arkiv.Models/Kodelister/MoetedeltakerFunksjonKoder.cs
@@ -0,0 +1,8 @@
+namespace KS.Fiks.Arkiv.Models.Kodelister
+{
+    public static class MoetedeltakerFunksjonKoder
+    {
+        public static readonly Kode Moeteleder = new Kode("M", "Møteleder");
+        public static readonly Kode Referent = new Kode("R", "Referent");
+    }
+}

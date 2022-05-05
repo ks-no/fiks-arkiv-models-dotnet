d5140465a892d7002dc39ac30c06d0c1f1b96e22
Jarle Børsheim <jarle.borsheim@gmail.com>
2022-05-05 11:45:29 +0200

    Tatt bort V1 i name

---
+++ KS.Fiks.Arkiv.Models.V1/Meldingstyper/FiksArkivPayloadHelper.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+
+namespace KS.Fiks.Arkiv.Models.V1.Meldingstyper
+{
+    public class FiksArkivPayloadHelper
+    {
+        public static HashSet<string> MeldingstyperMedPayload = new HashSet<string>
+        {
+            FiksArkivMeldingtype.Arkivmelding,
+            FiksArkivMeldingtype.ArkivmeldingKvittering,
+            FiksArkivMeldingtype.Sok,
+            FiksArkivMeldingtype.SokResultatMinimum,
+            FiksArkivMeldingtype.SokResultatNoekler,
+            FiksArkivMeldingtype.SokResultatUtvidet,
+            FiksArkivMeldingtype.JournalpostHentResultat,
+            FiksArkivMeldingtype.JournalpostHent,
+            FiksArkivMeldingtype.MappeHentResultat,
+            FiksArkivMeldingtype.MappeHent,
+            FiksArkivMeldingtype.DokumentfilHentResultat,
+            FiksArkivMeldingtype.DokumentfilHent
+        };
+        
+         public static string GetPayloadFilnavn(string messageType)
+        {
+            switch (messageType)
+            {
+                case FiksArkivMeldingtype.Arkivmelding:
+                case FiksArkivMeldingtype.DokumentfilHent:
+                case FiksArkivMeldingtype.MappeHent:
+                case FiksArkivMeldingtype.JournalpostHent:
+                    return "arkivmelding.xml";
+                case FiksArkivMeldingtype.ArkivmeldingKvittering:
+                    return "arkivmelding-kvittering.xml";
+                case FiksArkivMeldingtype.Sok:
+                    return "sok.xml";
+                case FiksArkivMeldingtype.SokResultatMinimum:
+                case FiksArkivMeldingtype.SokResultatNoekler:
+                case FiksArkivMeldingtype.SokResultatUtvidet:
+                case FiksArkivMeldingtype.JournalpostHentResultat:
+                case FiksArkivMeldingtype.DokumentfilHentResultat:
+                case FiksArkivMeldingtype.MappeHentResultat:
+                    return "resultat.xml";
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+         
+         public static bool HarPayload(string meldingstype)
+         {
+             return MeldingstyperMedPayload.Contains(meldingstype);
+         }
+    }
+}

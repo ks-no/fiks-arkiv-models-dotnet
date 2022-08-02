4fce6f4a4d322d1675a1c0f927ff6b82cd8ee496
Jarle Børsheim <jarle.borsheim@gmail.com>
2022-08-02 10:49:31 +0200

    La til manglende nye meldingstypen no.ks.fiks.arkiv.v1.arkivering.arkivmelding.oppdater.mottatt (#17)
    Co-authored-by: Jarle Børsheim <jarle.borsheim@sonat.no>

--- KS.Fiks.Arkiv.Models.V1/Meldingstyper/FiksArkivMeldingtype.cs
+++ KS.Fiks.Arkiv.Models.V1/Meldingstyper/FiksArkivMeldingtype.cs
@@ -7,6 +7,7 @@ namespace KS.Fiks.Arkiv.Models.V1.Meldingstyper
         // Arkivering
         public const string Arkivmelding = "no.ks.fiks.arkiv.v1.arkivering.arkivmelding";
         public const string ArkivmeldingOppdater = "no.ks.fiks.arkiv.v1.arkivering.arkivmelding.oppdater";
+        public const string ArkivmeldingOppdaterMottatt = "no.ks.fiks.arkiv.v1.arkivering.arkivmelding.oppdater.mottatt";
         public const string ArkivmeldingOppdaterKvittering = "no.ks.fiks.arkiv.v1.arkivering.arkivmelding.oppdater.kvittering";
         public const string ArkivmeldingMottatt = "no.ks.fiks.arkiv.v1.arkivering.arkivmelding.mottatt";
         public const string ArkivmeldingKvittering = "no.ks.fiks.arkiv.v1.arkivering.arkivmelding.kvittering";

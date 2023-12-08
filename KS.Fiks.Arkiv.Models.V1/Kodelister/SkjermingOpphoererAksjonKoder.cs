284330a9628cae6ad446a4a29854d0eb4211b68b
Jarle Børsheim <jarle.borsheim@sonat.no>
2023-12-08 13:56:45 +0100

    Ryddet i kodeliste hjelpeklassene

--- KS.Fiks.Arkiv.Models.V1/Kodelister/SkjermingOpphoererAksjonKoder.cs
+++ KS.Fiks.Arkiv.Models.V1/Kodelister/SkjermingOpphoererAksjonKoder.cs
@@ -5,6 +5,7 @@ namespace KS.Fiks.Arkiv.Models.V1.Kodelister
         public static readonly Kode Avgraderes = new Kode("A", "Avgraderes ved første kjøring av funksjon for avgradering etter at avgraderingstidspunktet er nådd");
         public static readonly Kode Gjennomgaas = new Kode("G", "Gjennomgås for vurdering av avgradering når angitt avgraderingstidspunkt nås");
         public static readonly Kode Sperrefrist = new Kode("S", "Sperrefrist, avgraderes automatisk når avgraderingsdato nås");
-        public static readonly Kode AvgraderingUtfoert = new Kode("AU", "Avgradering utført. Dette er den eneste avgraderingskoden som er tillatt etter at tilgangskode er slettet. ");
+        public static readonly Kode AvgraderingUtfoert = new Kode("AU", "Avgradering utført. Dette er den eneste avgraderingskoden som er tillatt etter at tilgangskode er slettet");
+        public static readonly Kode UnntattFraAutomatiskAvgradering = new Kode("U", "Unntatt fra automatisk avgradering");
     }
 }

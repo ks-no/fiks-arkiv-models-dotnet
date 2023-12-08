50fac3dc3685068c77d200f9e4e4528a6d638fcc
Jarle Børsheim <jarle.borsheim@gmail.com>
2023-12-08 14:06:41 +0100

    Ryddet i kodelister i henhold til det som ble gjort i spesifikasjonen (#25)
    * Ryddet i kodeliste i henhold til det som ble gjort i spesifikasjonen
    
    * Ryddet i kodeliste hjelpeklassene
    
    ---------
    
    Co-authored-by: Jarle Børsheim <jarle.borsheim@sonat.no>

--- KS.Fiks.Arkiv.Models.V1/Kodelister/MoetedeltakerFunksjonKoder.cs
+++ KS.Fiks.Arkiv.Models.V1/Kodelister/MoetedeltakerFunksjonKoder.cs
@@ -2,7 +2,10 @@ namespace KS.Fiks.Arkiv.Models.V1.Kodelister
 {
     public static class MoetedeltakerFunksjonKoder
     {
-        public static readonly Kode Moeteleder = new Kode("M", "Møteleder");
-        public static readonly Kode Referent = new Kode("R", "Referent");
+        public static readonly Kode Leder = new Kode("LEDER", "Leder");
+        public static readonly Kode Nestleder = new Kode("NESTL", "Nestleder");
+        public static readonly Kode Medlem = new Kode("MEDL", "Medlem");
+        public static readonly Kode Vara = new Kode("VARA", "Varamedlem");
+        public static readonly Kode Sekretaer = new Kode("SEKR", "Sekretær");
     }
 }
